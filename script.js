ab01fa7c36b83ad6352281c9e371720c4ec89eb1
RisetrokX <risetrok@gmail.com>
2026-06-29 23:19:52 +0200

    Switch to Stadia dark map for better road visibility

--- script.js
+++ script.js
@@ -255,9 +255,9 @@ function createMap() {
   }
 
   map = L.map('map', { zoomControl: true, scrollWheelZoom: true }).setView([20, 10], 2);
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+  L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
   userMarker = L.circleMarker([0, 0], {
